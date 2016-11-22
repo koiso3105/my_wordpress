f63514be7dce1aaab745d1ab63c29f2e81c3e48d
koiso <koiso@multieye.co.jp>
2016-11-22 22:29:22 +0900

    first_commit

---
+++ wp-content/themes/my_wordpress/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="keywords" content="キーワード,キーワード">
+<meta name="description" content="説明">
+</head>
+<body>
+<h1>ほげ</h1>
+<p>ほげ</p>
+</body>
+</html>
